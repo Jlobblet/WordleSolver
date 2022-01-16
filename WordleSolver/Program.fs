5cc4017ca1d3f04d4cd728b844951f758a5cd2ec
Jlobblet <jlobblet-github@gmx.co.uk>
2022-01-16 01:17:16 +0000

    Strategy graphing

--- WordleSolver/Program.fs
+++ WordleSolver/Program.fs
@@ -1,9 +1,14 @@
 open System
 open System.Diagnostics
 open System.IO
+open FSharpx.Collections
+open XPlot.Plotly
 
 let (<&&>) f g = fun x -> f x && g x
-let tap a x = a x; x
+
+let tap a x =
+    a x
+    x
 
 let rng = Random()
 
@@ -21,9 +26,11 @@ type LetterResult =
         fun (word: string) ->
             match result with
             | NotPresent -> not <| word.Contains character
-            | DifferentPlace -> word.Contains character && word[index] <> character
+            | DifferentPlace ->
+                word.Contains character
+                && word[index] <> character
             | Correct -> word[index] = character
-            
+
     static member FromGuess (word: string) index (character: char) =
         if word[index] = character then
             Correct
@@ -31,15 +38,22 @@ type LetterResult =
             DifferentPlace
         else
             NotPresent
-        
+
 [<StructuredFormatDisplay("{StructuredFormatDisplay}")>]
 type GuessResults =
     { Word: string
-      LetterResults: LetterResult[] }
+      LetterResults: LetterResult []
+      InitialLength: int }
     member this.StructuredFormatDisplay =
-        let blocks = String.concat "" (Array.map (fun lr -> lr.ToString()) this.LetterResults)
-        $"%s{this.Word} | %s{blocks}"
-    static member CreateFilter { Word = word; LetterResults = letterResults } =
+        let blocks =
+            String.concat "" (Array.map (fun lr -> lr.ToString()) this.LetterResults)
+
+        $"%s{this.Word} | %s{blocks} (from %i{this.InitialLength} options)"
+
+    static member CreateFilter
+        { Word = word
+          LetterResults = letterResults }
+        =
         Array.zip (word.ToCharArray()) letterResults
         |> Array.mapi LetterResult.CreateFilter
         |> Array.reduce (<&&>)
@@ -47,53 +61,101 @@ type GuessResults =
 type PlyResult =
     | Failed
     | Success of string
-    | Ongoing of string[]
-        
-let makeGuess (words: string[]) (answer: string) =
-    let guess =
-        words
-        |> Array.head
-        
+    | Ongoing of string []
+
+let makeGuess (picker: string [] -> string) (words: string []) (answer: string) =
+    let guess = picker words
     let letterResults =
         guess.ToCharArray()
         |> Array.mapi (LetterResult.FromGuess answer)
-        
-    { Word = guess
-      LetterResults = letterResults }
 
-let ply answer words =
-    let results = makeGuess words answer
-    printfn $"%A{results}"
-    
+    { Word = guess
+      LetterResults = letterResults
+      InitialLength = words.Length }
+
+let ply picker answer words =
+    let results = makeGuess picker words answer
+    //    printfn $"%A{results}"
+
     if Array.forall ((=) Correct) results.LetterResults then
         Success results.Word
     else
         let filter = GuessResults.CreateFilter results
-        Array.filter filter words
-        |> Ongoing
-    
-let solve answer words =
+        Array.filter filter words |> Ongoing
+
+let solve picker words answer =
     let rec inner words' i =
         match i with
-        | 6 -> Failed
+        //        | 6 -> Failed
         | _ ->
-            match ply answer words' with
+            match ply picker answer words' with
             | Ongoing remainingWords -> inner remainingWords (i + 1)
-            | s -> s
-        
+            | s -> i, s
+
     inner words 0
 
 let words =
     File.ReadAllLines "words.txt"
-    |> Array.map (fun s -> s.ToLower())
+    |> Array.Parallel.map (fun s -> s.ToLower())
     |> Array.distinct
-    |> Array.sortByDescending (fun s -> s.ToCharArray() |> Array.distinct |> Array.length)
-    |> Array.sortBy (fun _ -> rng.Next())
 
-let answer = words[rng.Next words.Length]
-printfn $"Answer is: %s{answer}"
+let randomWord (words: string []) = words[rng.Next words.Length]
+
+let mostCommonLetters (words: string []) =
+    let mapping =
+        words
+        |> Seq.collect id
+        |> Seq.countBy id
+        |> Map.ofSeq
+
+    words
+    |> Array.sortByDescending (Seq.sumBy (fun c -> mapping[c]))
+    |> Array.head
+    
+let leastCommonLetters (words: string []) =
+    let mapping =
+        words
+        |> Seq.collect id
+        |> Seq.countBy id
+        |> Map.ofSeq
+
+    words
+    |> Array.sortBy (Seq.sumBy (fun c -> mapping[c]))
+    |> Array.head
+    
+
+
+let strategies: (string * (string [] -> string)) [] =
+    [| "random", randomWord
+       "first", Array.head
+       "most common letters", mostCommonLetters
+       "least common letters", leastCommonLetters |]
+
+let n_runs = 1_000
+
+let doRun (label, picker) =
+    let xs, ys =
+        [| 1 .. n_runs |]
+        |> Array.Parallel.map
+            (fun _ ->
+                words[rng.Next words.Length]
+                |> solve picker words
+                |> fst)
+        |> Array.countBy id
+        |> Array.unzip
+
+    Bar(x = xs, y = ys, name = label)
+
 let sw = Stopwatch.StartNew()
-solve answer words |> ignore
-sw.Stop()
-printfn $"Solved in %i{sw.ElapsedMilliseconds}ms"
 
+strategies
+|> Array.Parallel.map doRun
+|> Chart.Plot
+|> Chart.WithLayout(Layout(barmode = "group"))
+|> Chart.WithTitle "Moves to complete"
+|> Chart.WithXTitle "Number of moves"
+|> Chart.WithYTitle "Frequency"
+|> Chart.Show
+
+sw.Stop()
+printfn $"%i{sw.ElapsedMilliseconds}ms"
